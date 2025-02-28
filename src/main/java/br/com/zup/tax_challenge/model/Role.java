4c9ae5f219985cfeec7161804c362ca2e7886882
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-02-28 15:56:28 -0300

    feat: add role class model with constructors

---
+++ src/main/java/br/com/zup/tax_challenge/model/Role.java
@@ -0,0 +1,22 @@
+package br.com.zup.tax_challenge.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Role() {
+    }
+}
