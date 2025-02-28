22d0c5bde53f559d3251e711bc1e1efa807c04df
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-02-28 15:26:07 -0300

    first commit

---
+++ src/main/java/br/com/zup/tax_challenge/TaxChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.tax_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/tax_challenge/TaxChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.tax_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
