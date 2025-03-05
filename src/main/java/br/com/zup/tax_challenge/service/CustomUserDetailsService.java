92789fcc8f272b320352345899826a4e04cd68a6
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-03-05 18:31:46 -0300

    feat: add loaduserbyusername method in customuserdetailsservice interface

--- src/main/java/br/com/zup/tax_challenge/service/CustomUserDetailsService.java
+++ src/main/java/br/com/zup/tax_challenge/service/CustomUserDetailsService.java
@@ -1,4 +1,7 @@
 package br.com.zup.tax_challenge.service;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
 public interface CustomUserDetailsService {
+    UserDetails loadUserByUsername(String username);
 }
