ddd68564f88f6ccafe7f33336a412269d86fac2c
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-03-05 17:57:48 -0300

    feat: create jwtauthenticationfilterclass with constructor

---
+++ src/main/java/br/com/zup/tax_challenge/infra/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,20 @@
+package br.com.zup.tax_challenge.infra.jwt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    private UserDetailsService userDetailsService;
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider, UserDetailsService userDetailsService) {
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.userDetailsService = userDetailsService;
+    }
+}
