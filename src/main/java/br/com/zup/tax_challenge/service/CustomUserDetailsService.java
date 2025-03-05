c2a15e15e5d7c0ead13599dba76bc9de7f2af34a
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-03-05 18:36:36 -0300

    fix: remove customuserdetailsservice interface and create customuserdetailsservice class

--- src/main/java/br/com/zup/tax_challenge/service/CustomUserDetailsService.java
+++ src/main/java/br/com/zup/tax_challenge/service/CustomUserDetailsService.java
@@ -1,7 +1,4 @@
 package br.com.zup.tax_challenge.service;
 
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface CustomUserDetailsService {
-    UserDetails loadUserByUsername(String username);
+public class CustomUserDetailsService {
 }
