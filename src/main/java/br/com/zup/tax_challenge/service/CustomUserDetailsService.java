d10a6465fb5ce90f059130619f7051da779e722e
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-03-05 18:51:50 -0300

    feat: add loaduserbyusername method in customuserdetailsservice class

--- src/main/java/br/com/zup/tax_challenge/service/CustomUserDetailsService.java
+++ src/main/java/br/com/zup/tax_challenge/service/CustomUserDetailsService.java
@@ -1,4 +1,37 @@
 package br.com.zup.tax_challenge.service;
 
-public class CustomUserDetailsService {
+import br.com.zup.tax_challenge.repository.UserRepository;
+import br.com.zup.tax_challenge.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUername(username) .orElseThrow(() ->
+                new UsernameNotFoundException("Usuário não existe"));
+
+        Set<GrantedAuthority> authorities = user.getRoles().stream()
+                .map((role) -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toSet());
+
+        return new org.springframework.security.core.userdetails.User(
+                username,
+                user.getPassword(),
+                authorities
+        );
+    }
 }
