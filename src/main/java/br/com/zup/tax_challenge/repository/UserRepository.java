8ccbc41c4535365d0357d6e781ae76f5ec27355a
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-03-05 18:52:48 -0300

    feat: add findbyusername method in userrepository

--- src/main/java/br/com/zup/tax_challenge/repository/UserRepository.java
+++ src/main/java/br/com/zup/tax_challenge/repository/UserRepository.java
@@ -3,6 +3,9 @@ package br.com.zup.tax_challenge.repository;
 import br.com.zup.tax_challenge.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUername(String username);
     boolean existsByUsername(String username);
 }
