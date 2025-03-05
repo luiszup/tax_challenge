4a0e657f3809b0dd8bf46741d3c86c594c1832e9
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-03-05 16:15:08 -0300

    feat: create userserviceimpl class in service package with registeruser method

---
+++ src/main/java/br/com/zup/tax_challenge/service/UserServiceImpl.java
@@ -0,0 +1,43 @@
+package br.com.zup.tax_challenge.service;
+
+import br.com.zup.tax_challenge.dto.RegisterUserDTO;
+import br.com.zup.tax_challenge.model.User;
+import br.com.zup.tax_challenge.model.Role;
+import br.com.zup.tax_challenge.repository.RoleRepository;
+import br.com.zup.tax_challenge.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class UserServiceImpl implements UserService{
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Override
+    public void registerUser(RegisterUserDTO registerUserDTO) {
+        if (userRepository.existsByUsername(registerUserDTO.getUsername())) {
+            throw new RuntimeException("O usuário já existe");
+        }
+
+        User user = new User();
+        user.setUsername(registerUserDTO.getUsername());
+        user.setPassword(passwordEncoder.encode(registerUserDTO.getPassword()));
+
+        Set<Role> roles = registerUserDTO.getRoles().stream()
+                .map(r -> new Role(r.name())).collect(Collectors.toSet());
+        roleRepository.saveAll(roles);
+
+        user.setRoles(roles);
+        userRepository.save(user);
+    }
+}
