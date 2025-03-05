3773afc6298e7edd6a1056e7a9eb59d4052f3a7a
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-03-05 17:50:18 -0300

    feat: create jwttokenprovider class with generatetoken, getusername and validatetoken methods

---
+++ src/main/java/br/com/zup/tax_challenge/infra/jwt/JwtTokenProvider.java
@@ -0,0 +1,41 @@
+package br.com.zup.tax_challenge.infra.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    private String jwtSecret = "d367ec6ce788f25f51e3749cdd3925cfae17a67d28ccafcfd8f08eb9b3c21be4";
+    private long jwtExpirationDate = 3600000;
+
+    public String generateToken(String username, String role) {
+        String token = Jwts.builder()
+                .setSubject(username)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationDate))
+                .signWith(SignatureAlgorithm.HS256, jwtSecret)
+                .compact();
+
+        return token;
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJwt(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJwt(token);
+        return true;
+    }
+}
