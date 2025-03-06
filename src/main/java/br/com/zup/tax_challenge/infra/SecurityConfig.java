8de5e2d486644793223eff536c1be473af283a69
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-03-06 09:23:05 -0300

    feat: create securityconfig class in infra package with passwordencoder method

---
+++ src/main/java/br/com/zup/tax_challenge/infra/SecurityConfig.java
@@ -0,0 +1,30 @@
+package br.com.zup.tax_challenge.infra;
+
+import br.com.zup.tax_challenge.infra.jwt.JwtAuthenticationEntryPoint;
+import br.com.zup.tax_challenge.infra.jwt.JwtAuthenticationFilter;
+import br.com.zup.tax_challenge.service.CustomUserDetailsService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@Component
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+
+    private CustomUserDetailsService userDetailsService;
+
+    private JwtAuthenticationEntryPoint authenticationEntryPoint;
+
+    private JwtAuthenticationFilter authenticationFilter;
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
