27df279ae39deed9bff9df11fd45cf05cd37a808
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-03-06 15:21:19 -0300

    feat: add login method in usercontroller class and authservice attribute

--- src/main/java/br/com/zup/tax_challenge/controller/UserController.java
+++ src/main/java/br/com/zup/tax_challenge/controller/UserController.java
@@ -1,7 +1,9 @@
 package br.com.zup.tax_challenge.controller;
 
+import br.com.zup.tax_challenge.dto.LoginDTO;
 import br.com.zup.tax_challenge.dto.RegisterUserDTO;
 import br.com.zup.tax_challenge.model.User;
+import br.com.zup.tax_challenge.service.AuthService;
 import br.com.zup.tax_challenge.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -18,13 +23,23 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    public UserController(UserService userService) {
+    @Autowired
+    private AuthService authService;
+
+    public UserController(UserService userService, AuthService authService) {
         this.userService = userService;
+        this.authService = authService;
     }
 
     @PostMapping("/registrar")
     public ResponseEntity<User> registerUser(@RequestBody RegisterUserDTO user) {
         User newUser = userService.registerUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> login(@RequestBody LoginDTO loginDTO) {
+        String token = authService.login(loginDTO);
+        return ResponseEntity.ok(Collections.singletonMap("token", token));
     }
 }
