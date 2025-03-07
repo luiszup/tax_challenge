e00e6d0f691744674810e0fe627a1518ac2ed5c9
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-03-07 10:06:43 -0300

    feat: create globalexceptionhandler class with illegalargumentexception handler

---
+++ src/main/java/br/com/zup/tax_challenge/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package br.com.zup.tax_challenge.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+}
