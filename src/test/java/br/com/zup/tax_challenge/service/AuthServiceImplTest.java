576c830c07186d82f6c2606350d4c9544d4dda9f
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-03-10 15:15:50 -0300

    feat: create authserviceimpltest class with loginsuccess test method

---
+++ src/test/java/br/com/zup/tax_challenge/service/AuthServiceImplTest.java
@@ -0,0 +1,61 @@
+package br.com.zup.tax_challenge.service;
+
+import br.com.zup.tax_challenge.dto.LoginDTO;
+import br.com.zup.tax_challenge.infra.jwt.JwtTokenProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class AuthServiceImplTest {
+    @Mock
+    private AuthenticationManager authenticationManager;
+
+    @Mock
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Mock
+    private Authentication authentication;
+
+    @InjectMocks
+    private AuthServiceImpl authService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void loginSuccess() {
+        LoginDTO loginDTO = new LoginDTO();
+        loginDTO.setUsername("usuarioteste");
+        loginDTO.setPassword("senha123");
+
+        String expectedToken = "token-teste";
+
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class)))
+                .thenReturn(authentication);
+        when(jwtTokenProvider.generateToken(authentication)).thenReturn(expectedToken);
+
+        SecurityContext securityContext = mock(SecurityContext.class);
+        SecurityContextHolder.setContext(securityContext);
+
+        String token = authService.login(loginDTO);
+
+        assertEquals(expectedToken, token);
+
+        verify(authenticationManager, times(1)).authenticate(any(UsernamePasswordAuthenticationToken.class));
+        verify(jwtTokenProvider, times(1)).generateToken(authentication);
+        verify(securityContext, times(1)).setAuthentication(authentication);
+    }
+}
