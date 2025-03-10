aea86ce1f3f8798e63ed4e9cbd5c8f03619e18b3
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-03-10 10:26:40 -0300

    refactor: changes type for dto in calculateimposto method in calculateimpostoserviceimpl class

--- src/main/java/br/com/zup/tax_challenge/service/CalculateImpostoServiceImpl.java
+++ src/main/java/br/com/zup/tax_challenge/service/CalculateImpostoServiceImpl.java
@@ -1,6 +1,7 @@
 package br.com.zup.tax_challenge.service;
 
 import br.com.zup.tax_challenge.dto.CalculateResponseDTO;
+import br.com.zup.tax_challenge.dto.TipoImpostoResponseDTO;
 import br.com.zup.tax_challenge.model.TipoImposto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,7 +20,7 @@ public class CalculateImpostoServiceImpl implements CalculateImpostoService{
 
     @Override
     public CalculateResponseDTO calculateImposto(Long tipoImpostoId, Double valorBase) {
-        TipoImposto imposto = tipoImpostoService.findTipoImposto(tipoImpostoId)
+        TipoImpostoResponseDTO imposto = tipoImpostoService.findTipoImposto(tipoImpostoId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Imposto não encontrado"));
 
         double valorImposto = valorBase * (imposto.getAliquota() / 100);
