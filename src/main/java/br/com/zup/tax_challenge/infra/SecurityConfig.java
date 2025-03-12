f7faa70624ac584ab703c0bc3a3938c51e14d5b1
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-03-12 13:36:25 -0300

    fix: refactor bcryptpasswordencoder bean type for bcryptpasswordencoder and add authenticationmanagerbean

--- src/main/java/br/com/zup/tax_challenge/infra/SecurityConfig.java
+++ src/main/java/br/com/zup/tax_challenge/infra/SecurityConfig.java
@@ -6,17 +6,16 @@ import br.com.zup.tax_challenge.service.CustomUserDetailsService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.stereotype.Component;
 
 @Configuration
-@Component
 @EnableWebSecurity
 @AllArgsConstructor
 public class SecurityConfig {
@@ -28,8 +27,13 @@ public class SecurityConfig {
     private JwtAuthenticationFilter authenticationFilter;
 
     @Bean
-    public static PasswordEncoder passwordEncoder() {
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
