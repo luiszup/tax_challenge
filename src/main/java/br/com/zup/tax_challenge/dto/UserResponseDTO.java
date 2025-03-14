a22a8f2d13fa03122267314bf9ab3c53be34ef0f
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-03-14 16:12:44 -0300

    feat: create userresponsedto class for json response

---
+++ src/main/java/br/com/zup/tax_challenge/dto/UserResponseDTO.java
@@ -0,0 +1,39 @@
+package br.com.zup.tax_challenge.dto;
+
+import java.util.Set;
+
+public class UserResponseDTO {
+    private Long id;
+    private String username;
+    private Set<String> roles;
+
+    public UserResponseDTO(Long id, String username, Set<String> roles) {
+        this.id = id;
+        this.username = username;
+        this.roles = roles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
+}
