41c6c8d9c7149490be1aed8e50f5e5a28a8ccb50
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-03-17 16:24:43 -0300

    refactor: claims the roles for authorization, changes the string type for role to a list and add getroles method in jwttokenprovider class

--- src/main/java/br/com/zup/tax_challenge/infra/jwt/JwtTokenProvider.java
+++ src/main/java/br/com/zup/tax_challenge/infra/jwt/JwtTokenProvider.java
@@ -1,12 +1,15 @@
 package br.com.zup.tax_challenge.infra.jwt;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
@@ -19,13 +22,13 @@ public class JwtTokenProvider {
 
         String username = authentication.getName();
 
-        String role = authentication.getAuthorities().stream()
+        List<String> roles = authentication.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.joining(","));
+                .collect(Collectors.toList());
 
         String token = Jwts.builder()
                 .setSubject(username)
-                .claim("role", role)
+                .claim("roles", roles)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationDate))
                 .signWith(SignatureAlgorithm.HS256, jwtSecret)
@@ -40,6 +43,18 @@ public class JwtTokenProvider {
                 .parseClaimsJws(token)
                 .getBody()
                 .getSubject();
+    }
+
+    public List<String> getRoles(String token) {
+        Claims claims = getClaims(token);
+        return claims.get("roles", List.class);
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody();
     }
 
     public boolean validateToken(String token) {
